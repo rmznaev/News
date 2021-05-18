6a58e65c7c877e85f8e8355ec15272631e72f264
Ramazan Abdullayev <ramazana@bankrespublika.az>
2021-05-18 09:45:08 +0400

    first commit

---
+++ News/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ramazan Abdullayev on 18.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
