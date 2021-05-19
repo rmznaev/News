efc10442833dfabf93581f9488738c8aafdfbf1e
Ramazan Abdullayev <ramazana@bankrespublika.az>
2021-05-19 10:02:50 +0400

    minor changes

--- News/View/NewsTableViewCell.swift
+++ News/View/NewsTableViewCell.swift
@@ -46,7 +46,7 @@ class NewsTableViewCell: UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented!")
     }
     
     override func layoutSubviews() {
